2b7104788decd74bc33094e4f6a23c8c76f611c1
joaoptgaino <joaoptgaino@hotmail.com>
2020-06-08 23:41:04 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css';
+import Routes from './routes'
+//JSX:Sintaxe de XML dentro JavaScript
+function App() {
+
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
